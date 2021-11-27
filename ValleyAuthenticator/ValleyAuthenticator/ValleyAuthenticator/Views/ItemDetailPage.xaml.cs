9582b106c55f3c1bf2276edfae242a17ca1dc446
Hielke <hielkehielkema93@gmail.com>
2021-11-27 09:42:43 +0100

    Add new Xamarin project

---
+++ ValleyAuthenticator/ValleyAuthenticator/ValleyAuthenticator/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using ValleyAuthenticator.ViewModels;
+using Xamarin.Forms;
+
+namespace ValleyAuthenticator.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
